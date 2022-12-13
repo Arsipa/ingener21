edb1efac83937237fae7a11ed3c6ef031c774360
Ko3JIuHa <nikitabrauner@yandex.ru>
2022-12-13 11:12:27 +0300

    border-radius,fix-slaider

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,30 +6,30 @@ const buttonRight = portfolioSlider.querySelector(".right");
 const curentSlideText = portfolioSlider.querySelector(".current");
 const sliderWidth = sliders[0].offsetWidth;
 const gap =
-    (sliderTrack.scrollWidth -
-        portfolioSlider.querySelector(".slide").offsetWidth * sliders.length) /
-    (sliders.length - 1);
+   (sliderTrack.scrollWidth -
+      portfolioSlider.querySelector(".slide").offsetWidth * sliders.length) /
+   (sliders.length - 1);
 const move = sliderWidth + gap;
 let position = 0;
 let currentSLide = 1;
 
 function updateSlider(translate, slideIndex = 1) {
-    position == 0
-        ? (buttonLeft.disabled = true)
-        : (buttonLeft.disabled = false);
-    position == -(move * (sliders.length - 1))
-        ? (buttonRight.disabled = true)
-        : (buttonRight.disabled = false);
+   position == 0
+      ? (buttonLeft.disabled = true)
+      : (buttonLeft.disabled = false);
+   position == -(move * (sliders.length - 1))
+      ? (buttonRight.disabled = true)
+      : (buttonRight.disabled = false);
 
-    curentSlideText.innerText = slideIndex;
-    sliderTrack.style.transform = `translateX(${translate}px`;
+   curentSlideText.innerText = slideIndex;
+   sliderTrack.style.transform = `translateX(${translate}px`;
 }
 
 buttonLeft.onclick = () => {
-    updateSlider(position += move, currentSLide-=1);
+   updateSlider(position += move, currentSLide -= 1);
 };
 buttonRight.onclick = () => {
-    updateSlider(position -= move, currentSLide+=1);
+   updateSlider(position -= move, currentSLide += 1);
 };
 
 portfolioSlider.querySelector(".total").innerText = sliders.length;
